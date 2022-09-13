ee2dd8cdecbb728a0f817020ee338c035cd798c6
Gydo-van-Hunen <9012373@student.tcrmbo.nl>
2022-09-13 12:06:10 +0200

    data types

---
+++ Data-types/app.js
@@ -0,0 +1,9 @@
+const MyName = "Gydo van Hunen ";
+let MyAge = 18;
+const Diploma = true;
+let passed;
+
+document.write(MyName)
+document.write(MyAge)
+document.write(Diploma)
+document.write(passed)
