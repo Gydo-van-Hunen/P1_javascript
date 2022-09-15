309fa7a54fced325216a5e30e23329450eab83a4
Gydo-van-Hunen <9012373@student.tcrmbo.nl>
2022-09-15 14:57:12 +0200

    opdracht_3.2-javascript

--- Data-types/app.js
+++ Data-types/app.js
@@ -1,9 +1,25 @@
-const MyName = "Gydo van Hunen ";
-let MyAge = 18;
-const Diploma = true;
-let passed;
+const MyName = "Gydo van Hunen "; //string
+let MyAge = 18; //number
+const Diploma = true; //boolean
+let passed; //unidentified
 
-document.write(MyName)
-document.write(MyAge)
-document.write(" ",Diploma)
-document.write(" ",passed)
+document.write(MyName);
+document.write(MyAge);
+document.write(" ",Diploma);
+document.write(" ",passed);
+
+let num1 = 90;
+let num2 = 45;
+
+let awnser1 = num1 + num2;
+let awnser2 = num1 - num2;
+let awnser3 = num1 * num2;
+let awnser4 = num1 / num2;
+let awnser5 = num1 % num2;
+
+console.log(awnser1)
+console.log(awnser2)
+console.log(awnser3)
+console.log(awnser4)
+console.log(awnser5)
+
