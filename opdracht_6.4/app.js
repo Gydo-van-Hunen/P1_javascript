4acb54a5161e6bb035f70b856e125bff676ac322
Gydo-van-Hunen <9012373@student.tcrmbo.nl>
2022-10-04 11:29:33 +0200

    Opzet opdracht_6.4

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,31 @@
+// oefening 1
+function caculate(num1, num2){
+    let awnser1 = num1 + num2;
+    console.log(awnser1);
+}
+
+caculate(1, 12);
+
+// oefening 2
+function multiplyByFive(num3){
+    let awnser2 = num3 * 5;
+    console.log(awnser2);
+}
+
+multiplyByFive(7);
+
+// oefening 3
+function checkInput(num4, num5){
+    let awnser3 = (num4 > num5) ? num4 : num5;
+    console.log(awnser3)
+}
+
+checkInput(9, 20);
+
+// oefening 4
+function caculateMinutes(num6){
+    let awnser4 = num6 / 60;
+    console.log(awnser4);
+}
+
+caculateMinutes(3600);
